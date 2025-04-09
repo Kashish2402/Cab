1d1d83cc85045e2ced515980e6f9f822d2da0d71
Kashish2402 <Kashishgupta2402@gmail.com>
2025-04-09 16:35:56 +0530

    fixed some typing error and install bcryptjs package

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true,limit:"50mb"}))
 app.use(cookieParser)
 
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.routes.js'
 
 app.use("/api/v1/users",userRouter)
 
