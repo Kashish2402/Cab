7c468c444acdf0974bd240fbf10a5c5c49c6395f
Kashish2402 <Kashishgupta2402@gmail.com>
2025-04-09 18:22:01 +0530

    updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true,limit:"50mb"}))
-app.use(cookieParser)
+app.use(cookieParser())
 
 import userRouter from './routes/user.routes.js'
 
