13ddb7ef60b668c14a3f2a893d821c448c42ba35
Kashish2402 <Kashishgupta2402@gmail.com>
2025-04-15 17:32:34 +0530

    updated ride controller

--- backend/src/controllers/ride.controllers.js
+++ backend/src/controllers/ride.controllers.js
@@ -18,22 +18,10 @@ const createRide = asyncHandler(async (req, res, next) => {
     userId: req.user?._id,
     pickupLocation,
     dropLocation,
-    rideTime: rideTime ? rideTime : Date.now,
-    rideDate: rideDate ? rideDate : Date.now,
+    rideTime: rideTime ? rideTime : Date.now(),
+    rideDate: rideDate ? rideDate : Date.now(),
+    status: "pending",
   });
-
-  const leader = await User.findOne({
-    role: "leader",
-    status: "active",
-    _id: { $ne: req.user?.id },
-  }).select("_id");
-
-  if (!leader)
-    return next(new ApiError(400, "No leader available for this ride"));
-
-  ride.leaderId = leader?._id;
-
-  await ride.save();
 
   const rideDetails = await Ride.aggregate([
     {
@@ -48,19 +36,11 @@ const createRide = asyncHandler(async (req, res, next) => {
       },
       $unwind: "$ride_Details",
     },
-    {
-      $lookup: {
-        from: "vehicles",
-        localField: "ride_Details.vehicleId",
-        foreignField: "_id",
-        as: "ride_Details_vehicle",
-      },
-      $unwind: "$ride_Details_vehicle",
-    },
+
     {
       $project: {
-        "ride_Details_vehicle.password": 0,
-        "ride_Details_vehicle.refreshToken": 0,
+        "ride_Details.password": 0,
+        "ride_Details.refreshToken": 0,
       },
     },
   ]);
@@ -68,6 +48,22 @@ const createRide = asyncHandler(async (req, res, next) => {
   return res
     .status(200)
     .json(new ApiResponse(200, rideDetails[0], "Ride created Successfully..."));
+});
+
+const acceptRide = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  if (!isValidObjectId(id)) return next(new ApiError(200, "Invalid ride id"));
+
+  const ride = await Ride.findById(id);
+  if (!ride) return next(new ApiError(200, "Ride not found"));
+  if (ride.status === "pending") {
+    ride.status = "accepted";
+    ride.leaderId = req.user?._id;
+  }
+  await ride.save();
+  return res
+    .status(200)
+    .json(new ApiResponse(200, ride, "Ride accepted successfully..."));
 });
 
 const getAllRides = asyncHandler(async (req, res, next) => {
@@ -198,7 +194,8 @@ const cancelRide = asyncHandler(async (req, res, next) => {
 
   if (!isValidObjectId(id)) return next(ApiError(400, "Ride doesn't exists!!"));
 
-  if (!reason) return next(new ApiError(400, "Cancellation reason is required"));
+  if (!reason)
+    return next(new ApiError(400, "Cancellation reason is required"));
 
   const ride = await Ride.findById(id);
   if (!ride) return next(ApiError(404, "Ride doesn't exists!!"));
@@ -214,11 +211,95 @@ const cancelRide = asyncHandler(async (req, res, next) => {
     .json(new ApiResponse(200, ride, "Ride cancelled Successfully..."));
 });
 
+const rideCompleted=asyncHandler(async(req,res,next)=>{
+  const{id}=req.params;
+
+  if(!isValidObjectId(id))return next(new ApiError(400,"Invalid RideId"))
+
+    const ride=await Ride.findById(id);
+
+    if (!ride) return next(new ApiError(404, "Ride not found"));
+
+    if (ride.status === "fulfilled") {
+      return next(new ApiError(400, "Ride is already completed"));
+    }
+
+    ride.status="fulfilled"
+    await ride.save();
+    return res
+    .status(200)
+    .json(new ApiResponse(200,ride,"Ride completed Successfully..."));
+})
+
+const getRideDetails=asyncHandler(async(req,res,next)=>{
+  const{id}=req.params;
+
+  if(!isValidObjectId(id))return next( new ApiError(400,"Invalid RideID !!!"))
+
+  const ride=await Ride.aggregate([
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId(id),
+        },
+
+    },
+    {
+      $lookup:{
+        from: "users",
+        localField: "userId",
+        foreignField: "_id",
+        as: "user",
+      },
+    },
+    {
+      $unwind:"$user"
+    },{
+      $project:{
+        "user.password":0,
+        "user.refreshToken":0
+    }},
+    {
+      $lookup:{
+        from:"users",
+        localField:"leaderId",
+        foreignField:"_id",
+        as:"leader"
+      }
+    },
+    {
+      $unwind: "$leader",
+      preserveNullAndEmptyArrays: true,
+    },
+    {
+      $project:{
+        "leader.password":0,
+        "leader.refreshToken":0
+        }
+    },
+    {
+      $lookup:{
+        from:"vehicles",
+        localField:"leader.vehicleId",
+        foreignField:"_id",
+        as:"vehicle"
+      }
+    },
+    {
+      $unwind: "$vehicle"
+    }
+  ])
+
+  if (!ride || ride.length === 0) return next(new ApiError(404, "Ride not found"));
+
+  return res.status(200).json(new ApiResponse(200, ride[0], "Ride details fetched successfully..."));
+})
 export {
   createRide,
+  acceptRide,
   getAllRides,
+  rideCompleted,
   getUserRides,
   getLeaderRides,
   updateRideStatus,
-  cancelRide
+  cancelRide,
 };
